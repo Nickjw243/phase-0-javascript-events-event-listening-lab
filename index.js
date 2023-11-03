3d8daf9aebf27eb35ef20a3e1e5afc93cb5d515b
Nick Weiland <njweiland23@gmail.com>
2023-11-02 19:56:18 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+// addingEventListener()
 function addingEventListener() {
-    const input = document.getElementById('button');
-
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert);
+const input = document.getElementById('button')
+input.addEventListener('click', function() {
+  alert('I was clicked')
+})
 }
 addingEventListener()
